1008168edb21e8297f9a573af90d80b3a7ae9172
DJCELL1 <Selwyn.lopati@gmail.com>
2025-12-11 15:52:52 +1300

    lopati

--- database/load_products.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import pandas as pd
-
-def load_products(db_path="products.db"):
-    con = sqlite3.connect(db_path)
-    df = pd.read_sql_query("SELECT * FROM products", con)
-    con.close()
-    return df
